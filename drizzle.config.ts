f9c8dea18a17ad297958cbdc65169522ff1fb4ef
HossGDev <stuckinasimulation@protonmail.com>
2026-08-11 16:37:19 -0700

    Change drizzle schema config to target all files via glob

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -10,7 +10,7 @@ if (!databaseUrl) {
 }
 
 export default defineConfig({
-  schema: "./src/lib/auth-schema.ts",
+  schema: "./src/lib/*-schema.ts",
   out: "./drizzle",
   dialect: "postgresql",
   dbCredentials: {
